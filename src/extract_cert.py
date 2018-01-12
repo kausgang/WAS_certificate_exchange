6d1c884f7b15025bc956a623087a15e1f878b33a
Kaustav Ganguli <write.to.kaustav@gmail.com>
2018-01-12 13:06:44 -0500

    removed keyStoreScope from extract_cert.py

--- src/extract_cert.py
+++ src/extract_cert.py
@@ -9,4 +9,4 @@ filename="cert_"+datestamp+".arm"
 #-------------------------------------------------------------------------------
 # extract certificate 
 #-------------------------------------------------------------------------------
-AdminTask.extractCertificate('[-certificateFilePath %s -base64Encoded true -certificateAlias default -keyStoreName CellDefaultKeyStore -keyStoreScope (cell):localhostCell01 ]' %filename)           
+AdminTask.extractCertificate('[-certificateFilePath %s -base64Encoded true -certificateAlias default -keyStoreName CellDefaultKeyStore ]' %filename)
